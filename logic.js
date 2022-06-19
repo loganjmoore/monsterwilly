f71469d203f26959106ffabb1df95b4dc4c55124
loganjmoore <loganjmoore@gmail.com>
2022-06-19 16:42:04 -0500

    randomized words working

--- logic.js
+++ logic.js
@@ -3,7 +3,7 @@ const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = (canvas.width = 1024);
 const CANVAS_HEIGHT = (canvas.height = 600);
 
-let gameSpeed = 3;
+let gameSpeed = -3;
 const backgroundLayer = new Image();
 backgroundLayer.src = './bg.jpg';
 const backgroundLayer2 = new Image();
@@ -14,13 +14,15 @@ truck.src = './truck.png';
 let x = -1224; // 0
 let x2 = 0; //1224
 
-if (false) {
+if (gameSpeed > 0) {
   x = 0;
   x2 = 1224;
 }
 
+let wordList = ['crash', 'up', 'down', 'dog'];
+
 let truckX = 200;
-let truckSpeed = 30;
+let truckSpeed = 40;
 
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
@@ -55,10 +57,10 @@ document.addEventListener(
   'keydown',
   (e) => {
     if (e.code == 'ArrowRight') {
-      //   gameSpeed = 3;
+      gameSpeed = 3;
       if (truckX < 550) truckX += truckSpeed;
     } else if (e.code == 'ArrowLeft') {
-      //   gameSpeed = -3;
+      gameSpeed = -3;
       if (truckX > 100) truckX -= truckSpeed;
     }
   },
@@ -69,10 +71,57 @@ document.addEventListener(
   'keyup',
   (e) => {
     if (e.code == 'ArrowRight') {
-      //   gameSpeed = 0;
+      gameSpeed = 0;
     } else if (e.code == 'ArrowLeft') {
-      //   gameSpeed = 0;
+      gameSpeed = 0;
     }
   },
   false
 );
+
+function play() {
+  let fiddler = new Audio('./fiddler.mp3');
+  fiddler.volume = 0.2;
+  fiddler.play();
+}
+
+var timer = setTimeout(timeToGuess, 10000);
+
+function timeToGuess() {
+  var wordNum = Math.floor(Math.random() * wordList.length);
+  var word = wordList[wordNum];
+  gameSpeed = 0;
+
+  console.log(word);
+
+  for (var w = 0; w < word.length; w++) {
+    console.log(word.charAt(w));
+    var nodeString =
+      "<a href='#'><div class='letter' id='letter_" +
+      w.toString() +
+      "' data='" +
+      word[w] +
+      "' onclick='guess(this);'>" +
+      word[w] +
+      '</div></a>';
+    document.getElementById('letter_panel').innerHTML += nodeString;
+    // var node = new DOMParser().parseFromString(nodeString, 'text/xml');
+    // document.getElementById('letter_panel').appendChild(node.documentElement);
+  }
+
+  //   let wordAudio = new Audio('./' + word + '.mp3').play();
+  //   wordAudio.volume = 1;
+  //   wordAudio.play;
+
+  setTimeout(timeToGuess, 10000);
+}
+
+let guessNumber = 1;
+function guess(guess) {
+  console.log(guess);
+  document.getElementById(
+    'entry_' + guessNumber.toString()
+  ).innerHTML = guess.getAttribute('data');
+  guess.style.display = 'none';
+  guessNumber++;
+}
